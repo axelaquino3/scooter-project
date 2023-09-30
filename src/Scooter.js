632fe7399ca6a7e0e09e9e1a757698dd6c60ac1b
axelaquino3 <axelaquino@google.com>
2023-09-30 00:02:12 -0400

    "Updated .js Files"

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,28 @@
-class Scooter{
-  // scooter code here
-}
+class Scooter {
 
+  static nextSerial = 1
+  
+  constructor(station){
+    this.station = station
+    this.user = null
+    this.serial = Scooter.nextSerial++
+    this.charge = 100
+    this.isBroken = false
+  }
+
+  rent(user) {
+    if(this.charge > 20 && this.isBroken === false) {
+      this.user = user
+      this.station = null
+    } else {
+      throw new Error("Scooter needs to charge or scooter needs repair")
+    }
+  }
+
+  dock(station){
+    this.station = station
+    this.user = null
+  }
+}
 
 module.exports = Scooter
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,39 @@
 const User = require('./User')
 const Scooter = require('./Scooter')
 
-class ScooterApp {
-  // ScooterApp code here
-}
+// class ScooterApp {
+//   // ScooterApp code here
+//   constructor(stations, registeredUsers){
+//     this.stations = {}
+//     this.registeredUsers = {}
+//   }
+
+//   registerUser(username, password, age) {
+//     if(username && age >= 18) {
+//       console.log("user has been registered")
+//       return username
+//     } else {
+//       throw new Error("already registered or too your young to register")
+//     }
+//   }
+
+//   loginUser(username, password){
+    
+//   }
+// }
+
+
+const scooter1 = new Scooter("Main")
+const scooter2 = new Scooter("Uptown")
+const user1 = new User("mike123", "hello123", 20)
+const user2 = new User("phil22", "123hello", 30)
+
+
+
+console.log(scooter1)
+console.log(scooter2)
+console.log(user1)
+console.log(user2)
+
 
 module.exports = ScooterApp
